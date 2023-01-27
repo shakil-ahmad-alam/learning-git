9d26bd8cbd8b9ad0f7fa147182fe9744a66352c9
shakilcode <helllo@shakilcode.com>
2023-01-27 19:23:10 +0530

    added logic of square of number

--- SampleOne.java
+++ SampleOne.java
@@ -1,4 +1,11 @@
+import java.util;
 public class SampleOne{
   public static void main(String args[]){
     System.out.println("hello world !");
+    Scanner scanner = new Scanner(System.in);
+    int x,y,z;
+    System.out.println("Enter any Integer : ");
+    x=scanner.nextInt();
+    y=x*x;
+    System.out.println("Square : "+y);
     }
