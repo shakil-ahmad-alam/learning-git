8829a3b5e979667a1dd8f46b7779c33ca51dbded
shakilcode <helllo@shakilcode.com>
2023-02-08 15:39:38 +0530

    thanks message added at local main branch

--- SampleTwo.java
+++ SampleTwo.java
@@ -2,5 +2,5 @@ class SampleTwo{
     public static void main(String[] args){
       System.out.println("This class has been created at main branch remote repostory...");
 	System.out.println("This statement added at local main branch!");
-
+	System.out.println("Thanks from local main branch !");
     }
