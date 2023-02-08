e8d2f77ba0761ff3c06657dead33ace9cc24475c
shakil-ahmad-alam <123544113+shakil-ahmad-alam@users.noreply.github.com>
2023-02-08 15:37:09 +0530

    Update SampleTwo.java
    Anas added statement at remote main branch

--- SampleTwo.java
+++ SampleTwo.java
@@ -1,6 +1,7 @@
 class SampleTwo{
     public static void main(String[] args){
       System.out.println("This class has been created at main branch remote repostory...");
-	System.out.println("This statement added at local main branch!");
+      System.out.println("This statement added at local main branch!");
+      System.out.println("This statement added at remote main branch by Anas !");	    
 
     }
