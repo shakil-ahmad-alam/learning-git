a462c766fef15e6a412bfd445862b0af2468715f
shakilcode <helllo@shakilcode.com>
2023-02-08 15:33:46 +0530

    new line added at local main branch

--- SampleTwo.java
+++ SampleTwo.java
@@ -1,4 +1,6 @@
 class SampleTwo{
     public static void main(String[] args){
       System.out.println("This class has been created at main branch remote repostory...");
+	System.out.println("This statement added at local main branch!");
+
     }
